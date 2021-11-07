0c5dacb329a861a0f8a068d1e8db005c286695fe
Devel <Devel1234!>
2021-11-07 22:41:30 +0100

    sprovozneni balicku

---
+++ source/resources/js/app.js
@@ -0,0 +1,7 @@
+import naja from 'naja';
+import netteForms from 'nette-forms';
+
+window.Nette = netteForms;
+
+document.addEventListener('DOMContentLoaded', naja.initialize.bind(naja));
+netteForms.initOnLoad();
