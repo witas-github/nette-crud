a30387d0cb9d8475588c2847fb9c01d8aeb74291
Devel <Devel1234!>
2021-11-08 12:27:22 +0100

    nettrine console

--- source/app/Presenters/HomepagePresenter.php
+++ source/app/Presenters/HomepagePresenter.php
@@ -4,22 +4,38 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
-use Nette\Database\Connection;
+use App\Entity\Project;
+use Datetime;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Persistence\ObjectRepository;
 use Nette;
+use Nettrine\ORM\EntityManagerDecorator;
 
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
-    private Connection $connection;
 
-    public function __construct(Connection $connection)
+
+    private EntityManagerDecorator $em;
+    private ObjectRepository $projectRepository;
+
+    public function __construct(EntityManagerDecorator $em)
     {
-        $this->connection = $connection;
+        $this->em = $em;
+        $this->projectRepository = $this->em->getRepository(Project::class);
     }
 
     public function renderDefault(){
 
-        print_r($this->connection->query("SHOW DATABASES;")->fetchAll());
+        $project = new Project();
+        $project->setName("Fajn projekt")
+            ->setDeadline(new Datetime())
+            ->setProjectType(Project::PROJECT_TYPE_TIME_LIMITED)
+            ->setWebProject(false);
+        $this->em->persist($project);
+        $this->em->flush();
+
+        dump($this->projectRepository->findAll());
         exit();
     }
 }
