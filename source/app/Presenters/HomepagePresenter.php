a58e5a9f673c51018a44c88d7cbdab7725259e09
Devel <Devel1234!>
2021-11-08 12:43:23 +0100

    nettrine console

--- source/app/Presenters/HomepagePresenter.php
+++ source/app/Presenters/HomepagePresenter.php
@@ -6,7 +6,6 @@ namespace App\Presenters;
 
 use App\Entity\Project;
 use Datetime;
-use Doctrine\ORM\EntityManager;
 use Doctrine\Persistence\ObjectRepository;
 use Nette;
 use Nettrine\ORM\EntityManagerDecorator;
@@ -14,19 +13,22 @@ use Nettrine\ORM\EntityManagerDecorator;
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
-
-
     private EntityManagerDecorator $em;
     private ObjectRepository $projectRepository;
 
+    /**
+     * @param EntityManagerDecorator $em
+     */
     public function __construct(EntityManagerDecorator $em)
     {
+        parent::__construct();
+
         $this->em = $em;
         $this->projectRepository = $this->em->getRepository(Project::class);
     }
 
-    public function renderDefault(){
-
+    public function renderDefault(): void
+    {
         $project = new Project();
         $project->setName("Fajn projekt")
             ->setDeadline(new Datetime())
